bc6bf5a3de860d468fd1b0b62c185b58c4c8ca31
Alexandr <jwchaganav@gmail.com>
2022-10-14 15:30:00 +0400

    задача 2

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите 2 числа, сначала первое: ");
+string? sNumber1 = Console.ReadLine();
+
+Console.Write("Теперь второе: ");
+string? sNumber2 = Console.ReadLine();
+
+int number1 = Convert.ToInt32(sNumber1);
+int number2 = Convert.ToInt32(sNumber2);
+
+if (number1>number2)
+    Console.WriteLine($"Число {number1} больше {number2}");
+else
+    Console.WriteLine($"Число {number2} больше {number1}");
